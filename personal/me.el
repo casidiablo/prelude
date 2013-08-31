93469f8d2c84bd8cbc947328b162f224b1044a6c
Cristian <me@cristian.io>
2013-08-31 01:16:59 -0700

    zzz

--- personal/me.el
+++ personal/me.el
@@ -7,5 +7,20 @@
 ;;; Code:
 
 (setq prelude-flyspell nil)
+(setq mac-option-modifier 'meta)
+
+(defun indent-buffer ()
+  (interactive)
+  (save-excursion
+    (indent-region (point-min) (point-max) nil)))
+(global-set-key [f12] 'indent-buffer)
+
+(defun my-c-mode ()
+  ;; change defaults
+  (setq-default indent-tabs-mode nil
+                tab-width 2
+                c-basic-offset 2))
+
+(setq prelude-c-mode-common-hook 'my-c-mode)
 
 ;;; me.el ends here
